d2fd39e41ff7a7fe7fd2683992764e8787b1d669
DESKTOP-JDS0IKJ\Егор <sekboyego111@gmail.com>
2023-03-03 23:05:17 +0500

    выполнил 5 уроков и сделал одно  задание)

---
+++ js/main.js
@@ -0,0 +1,6 @@
+       // document.write("Hello world by JS!!!");
+       // console.log("Hello World!");
+       // console.info("Hello World!");
+       // console.error("Hello World!");
+       // console.debug("Hello World!");
+     console.warn("Hello World!");
---
+++ js/zad1.js
@@ -0,0 +1,11 @@
+// var num = 5; // можно переопределить 
+// const numers = 99; // нельзя переопределить 
+// num = 7;
+// num_3 += 2 rofl
+// srt = Number("12") строку в число 
+// console.log("Переменная: ", num  + ".");
+// console.log(Math.PI);
+function multitype(a, b){
+    return a * b
+}
+console.log(multitype(3, 3))
